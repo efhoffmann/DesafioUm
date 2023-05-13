c8a034b487e3826f404e2780964bc94dd6b3b231
Eduardo Hoffmann <eduhoffmann.sistemas@yahoo.com.br>
2023-05-13 10:55:26 -0300

    projeto inicial

---
+++ DesafioUm/DesafioUmApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioUmApp.swift
+//  DesafioUm
+//
+//  Created by Eduardo Hoffmann on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DesafioUmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
